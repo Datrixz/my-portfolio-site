179af7f271e435d068c2d084dff150f7dc68a828
Pranjal Vaishnav <vsh.pranjal@gmail.com>
2022-04-08 22:18:08 +0530

    feat:added form validation in contact form

--- src/comp/Contact.js
+++ src/comp/Contact.js
@@ -6,9 +6,20 @@ import emailjs from "@emailjs/browser";
 import { useRef, useState, forwardRef } from "react";
 import Snackbar from "@mui/material/Snackbar";
 import MuiAlert from "@mui/material/Alert";
+import { useFormik } from "formik";
+import * as yup from "yup";
 
 const Alert = forwardRef(function Alert(props, ref) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+
+const validationSchema = yup.object({
+  name: yup.string().required("* Name field is required"),
+  email: yup
+    .string("Enter your email")
+    .email("Enter a valid email")
+    .required("* Email is required"),
+  text: yup.string().required("* Message field is required"),
 });
 
 const Contact = () => {
@@ -16,9 +27,8 @@ const Contact = () => {
   const upsm = useMediaQuery(theme.breakpoints.up("sm"));
   const [open, setOpen] = useState(false);
 
-  const handleClick = () => {
-    setOpen(true);
-  };
+  // const handleClick = () => {
+  // };
 
   const handleClose = (event, reason) => {
     if (reason === "clickaway") {
@@ -41,26 +51,54 @@ const Contact = () => {
   );
   const form = useRef();
 
-  const sendEmail = (e) => {
-    e.preventDefault();
+  // const sendEmail = (e) => {
+    // e.preventDefault();
 
-    emailjs
-      .sendForm(
-        "service_d98e9zh",
-        "template_jp0b5jn",
-        form.current,
-        "-GXuZJL3fcy4Xbg7k"
-      )
-      .then(
-        (result) => {
-          console.log(result.text);
-        },
-        (error) => {
-          console.log(error.text);
-        }
-      );
-    e.target.reset();
-  };
+    // emailjs
+    //   .sendForm(
+    //     "service_d98e9zh",
+    //     "template_jp0b5jn",
+    //     form.current,
+    //     "-GXuZJL3fcy4Xbg7k"
+    //   )
+    //   .then(
+    //     (result) => {
+    //       console.log(result.text);
+    //     },
+    //     (error) => {
+    //       console.log(error.text);
+    //     }
+    //   );
+    // e.target.reset();
+  // };
+
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      text: "",
+    },
+    validationSchema: validationSchema,
+    onSubmit: (values) => {
+      // comment below line
+      // alert(JSON.stringify(values, null, 2));
+
+      emailjs
+        .send(
+          "service_d98e9zh",
+          "template_jp0b5jn",
+          values,
+          "-GXuZJL3fcy4Xbg7k"
+        )
+        .then(
+          () => {
+            setOpen(true);
+            formik.setSubmitting(false);
+            formik.resetForm();
+          },
+        );
+    },
+  });
 
   return (
     <Box
@@ -73,25 +111,27 @@ const Contact = () => {
         justifyContent: "center",
       }}
     >
-       <Snackbar
-            anchorOrigin={{vertical: 'top', horizontal:'center'} }
-            open={open}
-            autoHideDuration={4000}
-            onClose={handleClose}
-          >
-            <Alert
-              onClose={handleClose}
-              severity="success"
-              sx={{ width: "100%" }}
-            >
-              Details Sent!
-            </Alert>
-          </Snackbar>
+      <Snackbar
+        anchorOrigin={{ vertical: "top", horizontal: "center" }}
+        open={open}
+        autoHideDuration={4000}
+        onClose={handleClose}
+      >
+        <Alert onClose={handleClose} severity="success" sx={{ width: "100%" }}>
+          Details Sent!
+        </Alert>
+      </Snackbar>
       <Fade top cascade>
         {contactLink}
 
         <Box sx={{ px: { xs: 1, sm: 2 }, maxWidth: "820px", mx: "auto" }}>
-          <Box ref={form} onSubmit={sendEmail} component="form" noValidate autoComplete="off">
+          <Box
+            ref={form}
+            onSubmit={formik.handleSubmit}
+            component="form"
+            autoComplete="off"
+            noValidate
+          >
             <TextField
               id="outlined-basic"
               label="Name"
@@ -99,6 +139,10 @@ const Contact = () => {
               variant="outlined"
               margin="normal"
               type="text"
+              value={formik.values.name}
+              onChange={formik.handleChange}
+              error={formik.touched.name && Boolean(formik.errors.name)}
+              helperText={formik.touched.name && formik.errors.name}
               fullWidth
               required
             />
@@ -108,6 +152,10 @@ const Contact = () => {
               name="email"
               variant="outlined"
               type="email"
+              value={formik.values.email}
+              onChange={formik.handleChange}
+              error={formik.touched.email && Boolean(formik.errors.email)}
+              helperText={formik.touched.email && formik.errors.email}
               fullWidth
               required
               sx={{
@@ -116,11 +164,15 @@ const Contact = () => {
             />
             <TextField
               id="outlined-basic"
-              label=""
+              label="Message"
               name="text"
               variant="outlined"
               margin="normal"
               type="text"
+              onChange={formik.handleChange}
+              value={formik.values.text}
+              error={formik.touched.text && Boolean(formik.errors.text)}
+              helperText={formik.touched.text && formik.errors.text}
               fullWidth
               multiline
               rows={upsm ? 8 : 5}
@@ -128,8 +180,8 @@ const Contact = () => {
             />
 
             <Button
-            type="submit"
-              onClick={handleClick}
+              type="submit"
+              disabled={formik.isSubmitting}
               variant="contained"
               color="primary"
               size="large"
@@ -138,7 +190,6 @@ const Contact = () => {
               Send
             </Button>
           </Box>
-         
         </Box>
       </Fade>
     </Box>
